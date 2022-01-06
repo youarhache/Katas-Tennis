f296653dc77a78c56fa9768a20a8059686717181
youarhache <youarhache@gmail.com>
2022-01-06 16:27:12 +0100

    score return Deuce when both players equal and above 3 points

--- tennis.py
+++ tennis.py
@@ -32,8 +32,8 @@ class TennisGame:
             self.player2.game_score += 1
 
     def score(self):
-        if self.player1.game_score == 3 and self.player2.game_score == 3:
+        if self.player1.game_score == self.player2.game_score >= 3:
             return "Deuce"
-        player1_score_name = self._score_names[self.player1.game_score]
-        player2_score_name = self._score_names[self.player2.game_score]
+        player1_score_name = self._score_names.get(self.player1.game_score)
+        player2_score_name = self._score_names.get(self.player2.game_score)
         return f"{self.player1.name}: {player1_score_name} - {self.player2.name}: {player2_score_name}"
--- test_tennis.py
+++ test_tennis.py
@@ -99,3 +99,12 @@ def test_score_when_both_player_at_3_then_deuce(new_game):
     current_score = new_game.score()
 
     assert current_score == "Deuce"
+
+
+def test_score_when_both_player_above_3_and_equal_then_deuce(new_game):
+    new_game.player1.game_score = 5
+    new_game.player2.game_score = 5
+
+    current_score = new_game.score()
+
+    assert current_score == "Deuce"
